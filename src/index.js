1673b222494916eff044f318b1d90fc1b3e5f8b9
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-01-24 10:19:08 +0530

    Blog App Created

--- src/index.js
+++ src/index.js
@@ -8,12 +8,18 @@ import {
  RouterProvider,
 } from "react-router-dom";
 import BlogsListPage from './pages/BlogsListPage';
+import BlogPage from './pages/BlogPage';
+
  
 const router = createBrowserRouter([
  {
   path: "/",
   element: <BlogsListPage />,
  },
+ {
+  path: "/blog",
+  element: <BlogPage />,
+ }
   
 ]);
  
